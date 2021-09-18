b8dd664ce6662b0c1efcab2ef58c594b72cb2133
ceden95 <90931092+ceden95@users.noreply.github.com>
2021-09-18 23:10:45 +0300

    Update and rename E_replacement.3.4.2.py to E_replacement.py

--- E_replacement.py
+++ E_replacement.py
@@ -3,6 +3,7 @@
 #the letters that are identical to the first letter of the sentance 
 #replacing with the letter "e".
 #the first letter remains the same.
+
 sen = input("hi please write a sentence:")
 new_sen = sen[0]+sen[1:].replace(sen[0], "e")
 print(new_sen)
