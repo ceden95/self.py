96cf276784022fed9c5b9b3a9fff527bc15fff3b
ceden95 <90931092+ceden95@users.noreply.github.com>
2021-09-18 11:58:04 +0300

    Update 3.4.3mission_capital.py

--- 3.4.3mission_capital.py
+++ 3.4.3mission_capital.py
@@ -1,6 +1,6 @@
 #the program gets a random word from user and prints back the word 
 #with capital letters at the second half of the word.
-#if number of letters is odd the second half 1 letter bigger.
+#if number of letters is odd; the second half is 1 letter bigger.
 
 word = input("please wright a random word that pops in your mind:")
 total = len(word)
