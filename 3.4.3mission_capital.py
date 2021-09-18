e9cfb4eb03c526638e4659a0197511e444541f63
ceden95 <90931092+ceden95@users.noreply.github.com>
2021-09-18 11:54:32 +0300

    Add files via upload

---
+++ 3.4.3mission_capital.py
@@ -0,0 +1,11 @@
+#the program gets a random word from user and prints back the word 
+#with capital letters at the second half of the word.
+#if number of letters is odd the second half 1 letter bigger.
+word = input("please wright a random word that pops in your mind:")
+total = len(word)
+number_of_caps = int(total/2)
+caps = word[number_of_caps: ]
+second_part = caps.upper()
+not_capital = (total)-(number_of_caps)
+first_part = word[:(number_of_caps)]
+print(first_part + second_part)
