9b5a06fc39aa2d940c17df925305613fd924f622
ceden95 <90931092+ceden95@users.noreply.github.com>
2021-09-20 16:53:18 +0300

    Add files via upload

---
+++ for_loop4.py
@@ -0,0 +1,21 @@
+#the program creates a new list without repetition of letters.
+#list made by user.
+
+def main():
+    my_str = input("type a word like 14 years old teenager(llikeeeee thhhatttt yooo): ")
+    sequence_del(my_str)
+    
+def sequence_del(my_str):
+    """creates a new string withoout repetition"""
+    prev_letter = ""
+    new_str = ""
+    for letter in my_str:
+        if letter == prev_letter:
+            continue
+        else:
+            new_str += letter
+        prev_letter = letter
+    print(new_str)
+        
+
+main()
